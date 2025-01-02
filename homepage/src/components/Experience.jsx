141ff86f01c2610836c6575031e71bd3e01776ee
Vighnesh1919 <vighneshbarage@gmail.com>
2025-01-02 23:37:23 +0530

    add project

--- homepage/src/components/Experience.jsx
+++ homepage/src/components/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="bg-[#0E0526] py-32 px-10" data-aos="slide-up" data-aos-duration="9000">
+    <div className="bg-[#0E0526] py-22 px-10" data-aos="slide-up" data-aos-duration="9000">
       <h2 className="text-4xl font-poppins-black text-center text-gray-800 mb-10">
         Trusted by Leading Companies
       </h2>
