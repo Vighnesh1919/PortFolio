fa312116eb6cc585765545a91c3327cdee54ead4
Vighnesh1919 <vighneshbarage@gmail.com>
2025-01-09 01:07:46 +0530

    add code

--- homepage/src/components/SocialMediaLinks.jsx
+++ homepage/src/components/SocialMediaLinks.jsx
@@ -3,25 +3,31 @@ import React from "react";
 const SocialMediaLinks = () => {
   return (
     <div className="flex justify-center space-x-6 mt-6">
+      {/* GitHub Icon */}
       <div className="icon">
         <a href="https://github.com/vighnesh1919" target="_blank" rel="noopener noreferrer">
-          <i className="fa-brands fa-github fa-2x text-white"></i>
+          <i className="fa-brands fa-github fa-2x text-white" title="GitHub"></i>
         </a>
       </div>
       
+      {/* LinkedIn Icon */}
       <div className="icon">
         <a href="https://www.linkedin.com/in/vighnesh-barage" target="_blank" rel="noopener noreferrer">
-          <i className="fa-brands fa-linkedin fa-2x text-white"></i>
+          <i className="fa-brands fa-linkedin fa-2x text-white" title="LinkedIn"></i>
         </a>
       </div>
+
+      {/* Email Icon */}
       <div className="icon">
         <a href="mailto:vighnesh@example.com" target="_blank" rel="noopener noreferrer">
-          <i className="fa-solid fa-envelope fa-2x text-white"></i>
+          <i className="fa-solid fa-envelope fa-2x text-white" title="Email"></i>
         </a>
       </div>
+
+      {/* LeetCode Icon */}
       <div className="icon">
         <a href="https://leetcode.com/vighnesh1919" target="_blank" rel="noopener noreferrer">
-          <i className="fa-solid fa-code fa-2x text-white"></i>
+          <i className="fa-brands fa-stack-overflow fa-2x text-white" title="LeetCode"></i> {/* Replacing with Stack Overflow logo as a placeholder */}
         </a>
       </div>
     </div>
